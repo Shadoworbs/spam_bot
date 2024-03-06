a6afe7a6f02cc24c9bd0c00e664e52f505e36790
Shadoworbs <saieedadam101@gmail.com>
2024-03-06 07:12:59 +0000

    added init.py

---
+++ init.py
@@ -0,0 +1,17 @@
+
+from pyrogram import Client, filters
+from config import api_id, api_hash
+
+api_id
+api_hash
+
+app = Client(name='my_account', api_id=api_id, api_hash=api_hash)
+
+@app.on_message(filters.text)
+async def echo(Client, message):
+    await message.reply(message.text)
+
+
+
+print("\n[+] Bot Started!\n[+] Press Ctrl+C to stop the bot.")
+app.start()
